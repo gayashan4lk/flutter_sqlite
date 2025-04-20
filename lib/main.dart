f75214002403651c8342875aac1e97ffa4cafb51
gayashan4lk <gayashan4lk@gmail.com>
2025-04-20 11:49:16 +0530

    Initialize SQLite database with Dog model and basic CRUD operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,50 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final db_path = await getDatabasesPath();
+  print(db_path);
+
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'doggie_db.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+      );
+    },
+    version: 1,
+  );
+
+  Future<void> insertDog(Dog dog) async {
+    final db = await database;
+    await db.insert(
+      'dogs',
+      dog.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Dog>> dogs() async {
+    final db = await database;
+
+    final List<Map<String, Object?>> queryResult = await db.query('dogs');
+
+    return [
+      for (final {'id': id as int, 'name': name as String, 'age': age as int}
+          in queryResult)
+        Dog(id: id, name: name, age: age),
+    ];
+  }
+
+  await insertDog(const Dog(id: 1, name: 'Buddy', age: 2));
+
+  print(await dogs());
+
   runApp(const MainApp());
 }
 
@@ -10,11 +54,24 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
+  }
+}
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  const Dog({required this.id, required this.name, required this.age});
+
+  Map<String, Object?> toMap() {
+    return {'id': id, 'name': name, 'age': age};
+  }
+
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
   }
 }
